5652103c4c52f8db499cdccf6a24c783c5e3c239
elenilda10 <126091743+elenilda10@users.noreply.github.com>
2026-04-26 23:24:24 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "Gols Flamengo | Bolão",
-  description: "Ranking oficial do bolão do FlamengoGolsBot",
+  description: "Ranking oficial do bolão FlamengoGolsBot",
+  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="pt-BR">
