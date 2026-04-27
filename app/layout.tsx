b5dbea59e93e13d6103e4d94bb069f52ef1a6079
elenilda10 <126091743+elenilda10@users.noreply.github.com>
2026-04-26 23:17:56 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Flamengo Ranking",
-  description: "Ranking do bolão do FlamengoGolsBot"
+  title: "Gols Flamengo | Bolão",
+  description: "Ranking oficial do bolão do FlamengoGolsBot",
 };
 
 export default function RootLayout({
